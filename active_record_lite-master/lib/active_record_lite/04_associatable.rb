3a6c383aa436ee382392cd618c6d589d3b1cb139
A. Zachary Allen <azacharyallen@gmail.com>
2014-04-18 15:36:48 -0700

    Implements Phase 4a: AssocOptions

--- active_record_lite-master/lib/active_record_lite/04_associatable.rb
+++ active_record_lite-master/lib/active_record_lite/04_associatable.rb
@@ -10,23 +10,33 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    self.class_name.constantize
   end
 
   def table_name
-    # ...
+    model_class::table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    default_options = {:foreign_key => "#{name}_id".to_sym, :class_name => "#{name.to_s.camelcase}", :primary_key => :id}
+    options = default_options.merge(options)
+
+    self.foreign_key = options[:foreign_key]
+    self.class_name = options[:class_name]
+    self.primary_key = options[:primary_key]
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+     default_options = {:foreign_key => "#{self_class_name.to_s.downcase}_id".to_sym, :class_name => "#{name.to_s.singularize.camelcase}", :primary_key => :id}
+     options = default_options.merge(options)
+
+    self.foreign_key = options[:foreign_key]
+    self.class_name = options[:class_name]
+    self.primary_key = options[:primary_key]
   end
 end
 
@@ -47,4 +57,17 @@ end
 
 class SQLObject
   # Mixin Associatable here...
+  extend Associatable
 end
+
+class Cat < SQLObject
+end
+
+m = HasManyOptions.new(:cats, :human, {})
+p m.class_name
+p m.foreign_key
+p m.primary_key
+
+p m.model_class
+p m.table_name
+
